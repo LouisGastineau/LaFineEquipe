3a9b7d454b5da118838c60428e6b2fca9bf0b2eb
Gaste <Gastineaulouis@gmail.com>
2025-04-01 23:07:46 +0200

    bootstrap c nul mais ca marche (pour l'instant)

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+import './bootstrap.js';
 
 /*
  * Welcome to your app's main JavaScript file!
@@ -6,6 +7,7 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const Encore = require('@symfony/webpack-encore');
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
-
+console.log("uwu");
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
@@ -13,7 +13,7 @@ Encore
     .setPublicPath('/build')
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
-
+    
     /*
      * ENTRY CONFIG
      *
@@ -24,6 +24,9 @@ Encore
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
+
+    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+    .enableStimulusBridge('./assets/controllers.json')
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
@@ -52,7 +55,7 @@ Encore
         config.useBuiltIns = 'usage';
         config.corejs = '3.38';
     })
-
+    .enableStimulusBridge('./assets/controllers.json')
     // enables Sass/SCSS support
     //.enableSassLoader()
 
@@ -71,3 +74,4 @@ Encore
 ;
 
 module.exports = Encore.getWebpackConfig();
+
